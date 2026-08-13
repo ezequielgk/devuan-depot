247ae35ea23c9ff8f7e498ec81a93ee58852b1e0
ezequielgk <rox737280@gmail.com>
2026-08-13 14:26:51 -0300

    chore: remove concord from update checker script

--- .github/scripts/check_updates.py
+++ .github/scripts/check_updates.py
@@ -6,7 +6,6 @@ import re
 CHECKS = [
     {"pkg": "niri", "type": "github_tag", "repo": "YaLTeR/niri"},
     {"pkg": "swayfx", "type": "github_tag", "repo": "WillPower3309/swayfx"},
-    {"pkg": "concord", "type": "github_tag", "repo": "chojs23/concord"},
     {"pkg": "foot", "type": "codeberg_tag", "repo": "dnkl/foot"},
     {"pkg": "xwayland-satellite", "type": "github_tag", "repo": "Supreeeme/xwayland-satellite"},
     {"pkg": "mullvad-libre", "type": "github_tag", "repo": "mullvad/mullvadvpn-app"},
