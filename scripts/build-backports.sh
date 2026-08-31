4f244f8aff584116b64bf0ee98a3c96dfd2ffe89
ezequielgk <rox737280@gmail.com>
2026-08-31 01:33:54 -0300

    fix(backports): patch debian/changelog to explicitly append ~devuandepot suffix to versions, preventing APT downgrade loops

--- scripts/build-backports.sh
+++ scripts/build-backports.sh
@@ -17,7 +17,16 @@ echo "Installing build dependencies for $DEBIAN_PKG..."
 apt-get build-dep -y "$DEBIAN_PKG/sid"
 
 echo "Downloading and compiling $DEBIAN_PKG..."
-apt-get source -b "$DEBIAN_PKG/sid"
+echo "Downloading source for $DEBIAN_PKG..."
+apt-get source "$DEBIAN_PKG/sid"
+DIR=$(find . -maxdepth 1 -mindepth 1 -type d)
+cd $DIR
+echo "Patching debian/changelog to append ~devuandepot tag..."
+sed -i "1s/)/~devuandepot)/" debian/changelog
+echo "Compiling $DEBIAN_PKG..."
+dpkg-buildpackage -b -uc -us
+cd ..
+
 
 echo "Moving generated debs..."
 mv *.deb ..
