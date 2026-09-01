b153554648005f2f0e984e7e01f8f1fe0427f9d0
ezequielgk <rox737280@gmail.com>
2026-09-01 12:45:04 -0300

    fix(gearlever): embed desktop-entry-lib pypi module into the deb package since it isn't listed in debian repos

--- scripts/build-gearlever.sh
+++ scripts/build-gearlever.sh
@@ -23,6 +23,10 @@ ninja -C build
 echo "Staging pkgroot..."
 mkdir -p ../../pkgroot/DEBIAN
 DESTDIR=$PWD/../../pkgroot ninja -C build install
+echo "Installing Python dependencies locally into the deb..."
+apt-get install -y python3-pip
+pip3 install desktop-entry-lib --target=$PWD/../../pkgroot/usr/lib/python3/dist-packages/ --no-user
+
 
 cd ../..
 
